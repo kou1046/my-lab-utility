3eebb5295ae85ffdc17dc8d818e58e57edc16ee7
kou1046 <iwashiro0517@yahoo.co.jp>
2022-09-17 15:40:37 +0900

    containermanagerにおいて，receive_funcの引数追加

--- mywidget.py
+++ mywidget.py
@@ -8,16 +8,16 @@ import cv2
 
 Value = TypeVar('Value')
 class ContainerManager(tk.LabelFrame, metaclass=ABCMeta): #抽象クラス
-    def __init__(self, master, contents:Container[Value], receive_content_func:Callable[[Value], None], side:str='top', **kw): 
+    def __init__(self, master, contents:Container[Value], receive_content_func:Callable[[tk.Event, Value], None], side:str='top', **kw): 
         """
-        receive_content_funcに与えられる引数はcontents内の要素.
-        継承先の_create_content_widget内で作成したウィジェットのイベント(ボタンクリックなど)時に要素が渡される．
+        receive_content_funcに与えられる引数はイベントとcontents内の要素.
+        継承先の_create_content_widget内で作成したウィジェットのバインド時に設定する.
         """
         super().__init__(master, **kw)
         self.contents:Container[Value] = contents
         self.receive_content_func:Callable[[Value], None] = receive_content_func
         self.side:str = side
-        self.update(side)       
+        self.update()       
     def update(self): #クラスの外側やreceive_content_func内でcontentsを更新したときはこのメソッドを呼ぶとウィジェットが更新される
         for widget in self.winfo_children():
             widget.destroy()
